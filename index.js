71de575fe563d43a21a8ee5fce52057215e4dc28
Wilson Fong <wilsonfong1002@outlook.com>
2021-10-19 20:18:32 -0400

    add port

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`🚀  Server listening on port ${port}`);
+});
 const api = express.Router();
+
+
 
 api.post('/webhook/getCustomerData', (req, res) => {
   console.log('Webhook event:', req.body)
